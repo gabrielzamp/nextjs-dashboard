f711e663efea644d2244ad7496610ca5a266451f
gabrielzamp <gzampierimkt@gmail.com>
2024-01-30 14:42:26 -0300

    chapter 11

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image';
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
+    <main className="noise3 flex min-h-screen flex-col p-3">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
       </div>
